356a899aed849455bc37b76f4032879eac6a5fb0
blixondi <101459072+blixondi@users.noreply.github.com>
2023-03-23 20:57:59 +0700

    Finish homework

--- app/src/main/java/com/shem/adv160420033week4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/shem/adv160420033week4/view/StudentDetailFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 import com.shem.adv160420033week4.R
-
+import com.shem.adv160420033week4.viewmodel.DetailViewModel
 
 
 class StudentDetailFragment : Fragment() {
+    private lateinit var viewModel: DetailViewModel
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,4 +23,22 @@ class StudentDetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_student_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        val txtID = view.findViewById<TextView>(R.id.txtID)
+        val txtStudentName = view.findViewById<TextView>(R.id.txtStudentName)
+        val txtDoB = view.findViewById<TextView>(R.id.txtBod)
+        val txtPhone = view.findViewById<TextView>(R.id.txtPhone)
+
+        viewModel.studentLD.observe(viewLifecycleOwner) { student ->
+            txtID.text = student.id
+            txtStudentName.text = student.name
+            txtDoB.text = student.dob
+            txtPhone.text = student.phone
+        }
+    }
 }
