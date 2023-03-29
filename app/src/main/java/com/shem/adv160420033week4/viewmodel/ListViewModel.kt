19e7301f5d8668aa4616ec34915a1d0cd65ca62d
blixondi <blixondi@gmail.com>
2023-03-29 13:34:57 +0700

    fetch data from API

--- app/src/main/java/com/shem/adv160420033week4/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/shem/adv160420033week4/viewmodel/ListViewModel.kt
@@ -1,27 +1,27 @@
 package com.shem.adv160420033week4.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
 import com.shem.adv160420033week4.model.Student
 
-class ListViewModel: ViewModel() {
+class ListViewModel(Application: Application): AndroidViewModel(Application) {
     val studentsLD = MutableLiveData<ArrayList<Student>>()
     val studentLoadErrorLD = MutableLiveData<Boolean>()
     val loadingLD = MutableLiveData<Boolean>()
 
+    val TAG = "volleyTag"
+    private var queue: RequestQueue? = null
+
     fun refresh() {
-        val student1 =
-            Student("16055","Nonie","1998/03/28","5718444778","http://dummyimage.com/75x100.jpg/cc0000/ffffff")
-
-        val student2 =
-            Student("13312","Rich","1994/12/14","3925444073","http://dummyimage.com/75x100.jpg/5fa2dd/ffffff")
-
-        val student3 =
-            Student("11204","Dinny","1994/10/07","6827808747","http://dummyimage.com/75x100.jpg/5fa2dd/ffffff1")
-        val studentList:ArrayList<Student> = arrayListOf<Student>(student1, student2, student3)
-        studentsLD.value = studentList
+        loadingLD.value = true
         studentLoadErrorLD.value = false
-        loadingLD.value = false
+        queue = Volley.newRequestQueue(getApplication())
+        val url = "http://adv.jitusolution.com/student.php"
+
     }
 
 }
