4cce5f1b8417004deb1db5af5415491beef7c871
blixondi <blixondi@gmail.com>
2023-04-05 13:28:54 +0700

    add error icon to project and load image to util.kt

--- app/src/main/java/com/shem/adv160420033week4/util/util.kt
+++ app/src/main/java/com/shem/adv160420033week4/util/util.kt
@@ -2,7 +2,14 @@ package com.shem.adv160420033week4.util
 
 import android.widget.ImageView
 import android.widget.ProgressBar
+import com.shem.adv160420033week4.R
+import com.squareup.picasso.Picasso
 
 fun ImageView.loadImage(url: String?, progressBar: ProgressBar){
-
+    Picasso.get()
+        .load(url)
+        .resize(400, 400)
+        .centerCrop()
+        .error(R.drawable.baseline_error_24)
+        .into(this)
 }
