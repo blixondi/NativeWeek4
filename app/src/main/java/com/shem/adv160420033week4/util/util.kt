09e67bf98d74d95da6077179d6f73b54c360d7e3
blixondi <blixondi@gmail.com>
2023-04-05 13:26:23 +0700

    add picasso 2.8 dependency

--- app/src/main/java/com/shem/adv160420033week4/util/util.kt
+++ app/src/main/java/com/shem/adv160420033week4/util/util.kt
@@ -1,2 +1,8 @@
 package com.shem.adv160420033week4.util
 
+import android.widget.ImageView
+import android.widget.ProgressBar
+
+fun ImageView.loadImage(url: String?, progressBar: ProgressBar){
+
+}
